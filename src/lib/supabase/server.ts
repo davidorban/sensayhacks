deac7c06af286811b90f627422b17129a9f2a942
David Orban <david@davidorban.com>
2025-03-28 17:47:18 +0100

    fix: Suppress no-unused-vars lint for catch blocks

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -16,14 +16,14 @@ export function createServerComponentClient() {
         set(name: string, value: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value, ...options })
-          } catch (_error) {
+          } catch (_error) { // eslint-disable-next-line @typescript-eslint/no-unused-vars
             // Server Components cannot set cookies directly
           }
         },
         remove(name: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value: '', ...options })
-          } catch (_error) {
+          } catch (_error) { // eslint-disable-next-line @typescript-eslint/no-unused-vars
             // Server Components cannot delete cookies directly
           }
         },
